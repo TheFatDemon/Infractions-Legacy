b6ffd21d2f220c72fc39ef9b323a3649df54e8fd
HmmmQuestionMark <support@clashnia.com>
2012-11-26 14:14:21 -0500

    Small fixes.

--- src/com/legit2/hqm/Infractions/Infractions.java
+++ src/com/legit2/hqm/Infractions/Infractions.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.scheduler.BukkitWorker;
 
-import com.legit2.hqm.ClashniaUpdate.Update;
+import com.clashnia.ClashniaUpdate.Update;
 
 public class Infractions extends JavaPlugin implements Listener {
 	public static Logger log = Logger.getLogger("Minecraft");
--- src/com/legit2/hqm/Infractions/Manager.java
+++ src/com/legit2/hqm/Infractions/Manager.java
@@ -8,7 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import com.legit2.hqm.ClashniaUpdate.Update;
+import com.clashnia.ClashniaUpdate.Update;
 
 public class Manager implements Listener {
 	/*
