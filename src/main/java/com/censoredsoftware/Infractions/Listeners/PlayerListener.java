7c71256144e165127fa6e546f16f18610a58b7a2
HmmmQuestionMark <ajchauncey@gmail.com>
2013-11-27 23:28:44 -0500

    Work towards version 0.4.6.

--- src/main/java/com/censoredsoftware/Infractions/Listeners/PlayerListener.java
+++ src/main/java/com/censoredsoftware/Infractions/Listeners/PlayerListener.java
@@ -11,36 +11,35 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.logging.Logger;
 
-public class PlayerListener implements Listener
-{
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent e)
-	{ // sync to master file
-		final Player p = e.getPlayer();
-		if(SettingUtil.getSettingBoolean("motd")) p.sendMessage("This server is policed with infractions.");
-		if(SaveUtil.hasData(p, "NEWINFRACTION"))
-		{
-			if((Boolean) SaveUtil.getData(p, "NEWINFRACTION"))
-			{
-				p.sendMessage(ChatColor.RED + "You have a new infraction!" + ChatColor.WHITE + " Use " + ChatColor.YELLOW + "/history" + ChatColor.WHITE + " for more information.");
-				SaveUtil.saveData(p, "NEWINFRACTION", false);
-			}
-		}
-		if(SaveUtil.hasData(p, "NEWVIRTUE"))
-		{
-			if((Boolean) SaveUtil.getData(p, "NEWVIRTUE"))
-			{
-				p.sendMessage(ChatColor.RED + "You have a new virtue!" + ChatColor.WHITE + " Use " + ChatColor.YELLOW + "/history" + ChatColor.WHITE + " for more information.");
-				SaveUtil.saveData(p, "NEWVIRTUE", false);
-			}
-		}
-		if(!SaveUtil.hasPlayer(p))
-		{
-			Logger.getLogger("Minecraft").info("[Infractions] " + p.getName() + " joined for the first time.");
-			SaveUtil.addPlayer(p);
-		}
+public class PlayerListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) { // sync to master file
+        final Player p = e.getPlayer();
+        if (SettingUtil.getSettingBoolean("motd")) p.sendMessage("This server is policed with infractions.");
+        if (p.hasPermission("infractions.ignore")) {
+            if (MiscUtil.getInfractionsList(p.getName()) != null)
+                for (String infractionID : MiscUtil.getInfractionsList(p.getName()))
+                    MiscUtil.removeInfraction(p.getName(), infractionID);
+            return;
+        }
+        if (SaveUtil.hasData(p, "NEWINFRACTION")) {
+            if ((Boolean) SaveUtil.getData(p, "NEWINFRACTION")) {
+                p.sendMessage(ChatColor.RED + "You have a new infraction!" + ChatColor.WHITE + " Use " + ChatColor.YELLOW + "/history" + ChatColor.WHITE + " for more information.");
+                SaveUtil.saveData(p, "NEWINFRACTION", false);
+            }
+        }
+        if (SaveUtil.hasData(p, "NEWVIRTUE")) {
+            if ((Boolean) SaveUtil.getData(p, "NEWVIRTUE")) {
+                p.sendMessage(ChatColor.RED + "You have a new virtue!" + ChatColor.WHITE + " Use " + ChatColor.YELLOW + "/history" + ChatColor.WHITE + " for more information.");
+                SaveUtil.saveData(p, "NEWVIRTUE", false);
+            }
+        }
+        if (!SaveUtil.hasPlayer(p)) {
+            Logger.getLogger("Minecraft").info("[Infractions] " + p.getName() + " joined for the first time.");
+            SaveUtil.addPlayer(p);
+        }
 
-		SaveUtil.saveData(p, "LASTLOGINTIME", System.currentTimeMillis());
-		MiscUtil.checkScore(p);
-	}
+        SaveUtil.saveData(p, "LASTLOGINTIME", System.currentTimeMillis());
+        MiscUtil.checkScore(p);
+    }
 }
